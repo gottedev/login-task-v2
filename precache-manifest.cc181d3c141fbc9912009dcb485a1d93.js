c3aa4812b353bb9ea2f7a04fcfef5f163750ea3b
gottedev <gottedev@gmail.com>
2020-08-27 14:12:56 +0100

    Updates

---
+++ precache-manifest.cc181d3c141fbc9912009dcb485a1d93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcfbadc26d839a9ac82b902e9b61909",
+    "url": "/login-task-v2/index.html"
+  },
+  {
+    "revision": "164802ad9d05fa638fbb",
+    "url": "/login-task-v2/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "21f2569c7a7191f1adb1",
+    "url": "/login-task-v2/static/css/main.72679223.chunk.css"
+  },
+  {
+    "revision": "164802ad9d05fa638fbb",
+    "url": "/login-task-v2/static/js/2.f1e613f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/login-task-v2/static/js/2.f1e613f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f2569c7a7191f1adb1",
+    "url": "/login-task-v2/static/js/main.027576a5.chunk.js"
+  },
+  {
+    "revision": "36a992fe4924ec4953f5",
+    "url": "/login-task-v2/static/js/runtime-main.881d196d.js"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/login-task-v2/static/media/Gotham.b54724f5.ttf"
+  },
+  {
+    "revision": "9da97edd306f798b1cb2eb6dca334e22",
+    "url": "/login-task-v2/static/media/logo.9da97edd.svg"
+  }
+]);
